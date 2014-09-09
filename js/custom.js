0e2d5e5047b0a46eca0d615f9c2ac6abc9174d3c
webcrawla <webcrawl21@gmail.com>
2014-09-09 20:07:37 +1000

    Imported all site files - first-time

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+
+$('body').scrollspy({ target: '.navbar-collapse'})
+
+$(".navbar-collapse ul li a[href^='#']").on('click', function (e) {
+	//prevent default anchor click behavior
+	e.preventDefault();
+	//store hash
+	var hash = this.hash;
+	//animate
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top -50
+	},700, function(){
+		//well done. add hash to url
+		// (default click behavior)
+		window.location.hash = hash;
+	
+	});
+});
+
+$("a.navbar-brand[href^='#']").on('click', function (e) {
+	//prevent default anchor click behavior
+	e.preventDefault();
+	//store hash
+	var hash = this.hash;
+	//animate
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top -50
+	},700, function(){
+		//well done. add hash to url
+		// (default click behavior)
+		window.location.hash = hash;
+	
+	});
+});
+
+$("#back-to-top a[href^='#']").on('click', function (e) {
+	//prevent default anchor click behavior
+	e.preventDefault();
+	//store hash
+	var hash = this.hash;
+	//animate
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top -50
+	},700, function(){
+		//well done. add hash to url
+		// (default click behavior)
+		window.location.hash = hash;
+	
+	});
+});
